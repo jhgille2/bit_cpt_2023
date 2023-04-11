eadd6ad9b386a2632f8a4499abc54e040a3ba9ce
Jay Gillenwater <55106933+jhgille2@users.noreply.github.com>
2023-04-11 15:06:09 -0400

    Update make_folders.sh

--- make_folders.sh
+++ make_folders.sh
@@ -1,3 +1,2 @@
-mkdir AlignedToTranscriptome salmon_align_quant transcriptome data multiqc_reports fastqc starindices starOutputfiles
+mkdir AlignedToTranscriptome salmon_align_quant transcriptome fastqc starindices starOutputfiles
 
-mkdir multiqc_reports/fastqc
